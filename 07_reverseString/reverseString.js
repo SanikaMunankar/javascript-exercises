2506573ca59809489bbd82540a00e5123f9dbc04
Sanika Munankar <97614048+SanikaMunankar@users.noreply.github.com>
2025-12-01 15:30:52 +0530

    Complete '07_reverseString' exercise
    Learned how to split and join arrays. Dealt with sentences. Also learned reverse() which only works on arrays. Learned to reverse strings.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let stringArray = word.split('');
+    let newStringArray = stringArray.reverse();
+    let newString = newStringArray.join('');
+    return newString;
 };
 
 // Do not edit below this line
