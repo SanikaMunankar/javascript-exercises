c9ed23665631544e55af34ac262b576efb752207
Sanika Munankar <97614048+SanikaMunankar@users.noreply.github.com>
2025-12-08 21:12:07 +0530

    Complete '09_sumAll' exercise
    Learned to use typeof() function to exclude certain variables.
    This program takes two numbers and then adds the number between them including both numbers.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (typeof(start) !== 'number' || typeof(end) !== 'number'){
+        return "ERROR";
+    }
+    else if (start < 0 || end < 0){
+        return "ERROR";
+    }
+    else if(start % 1 !== 0 || end % 1 !== 0){
+        return "ERROR";
+    }
+    else{
+        if (start>end){
+            startNum = end;
+            endNum = start;
+        }
+        else{
+            startNum = start;
+            endNum = end;
+        }
+        
+        for (startNum; startNum<=endNum; startNum++){
+            sum += startNum;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
