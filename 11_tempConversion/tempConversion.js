31f942f51db7642ba2f0049cfc7611a105de8c40
Sanika Munankar <97614048+SanikaMunankar@users.noreply.github.com>
2025-12-10 13:10:45 +0530

    Complete '11_tempConversion' exercise
    Created two function that converts fahrenheit to celcius and celcius to fahrenheit.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const celcius = (temp-32)*5/9;
+  const roundedCelcius = Math.round(celcius*10)/10;
+  return roundedCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrenheit = temp*9/5+32;
+  const roundedFahrenheit = Math.round(fahrenheit*10)/10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
